c408d06fc0a184dd548f0cea34b80e3a8bbc3653
Shivam Yadav <sy035789@gmail.com>
2026-04-07 10:48:13 +0530

    fixed CORS

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://restaurant-one-beryl-78.vercel.app"],
     methods: ["POST"],
     credentials: true,
   })
