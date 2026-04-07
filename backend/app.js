8ac948f85b41d20a3a62520b0a9f7dafaf644413
Shivam Yadav <sy035789@gmail.com>
2026-04-07 20:18:28 +0530

    fix mongodb connection timeout

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,9 @@ import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./error/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 
+dotenv.config({ path: "./config/config.env" }); // ← moved to top
+
 const app = express();
-dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
@@ -23,6 +24,6 @@ app.use("/api/v1/reservation", reservationRouter);
 
 dbConnection();
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 export default app;
--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -4,11 +4,15 @@ export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
       dbName: "RESERVATIONS",
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      ssl: true,
+      tls: true,
     })
     .then(() => {
       console.log("Connected to database!");
     })
     .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`);
+      console.log(`Some error occured while connecting to database: ${err}`);
     });
 };
